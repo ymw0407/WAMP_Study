85485fbff8fcf48ea621ffee4257f8791271b17b
Yun Min Woo <77202633+ymw0407@users.noreply.github.com>
2022-08-03 23:24:40 +0900

    Delete htdocs directory

--- htdocs/prt/create_process.php
+++
@@ -1,4 +0,0 @@
-<?php
-file_put_contents('data/'.$_POST['title'], $_POST['description']);
-header('Location: /prt\prt.php?id='.$_POST['title']);
-?>
--- htdocs/prt/delete_process.php
+++
@@ -1,4 +0,0 @@
-<?php
-unlink('data/'.basename($_POST['id']));
-header('Location: /prt\prt.php')
-?>
